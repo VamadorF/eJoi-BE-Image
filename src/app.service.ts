7beabae14842f5c57437e5ea6fef5c54a14e3905
Javier Fernandez <javier.fernandez@mejoradigital.cl>
2026-03-11 17:15:11 -0300

    feat: hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return { message: 'Hello IA Images!', status: 'success', data: null, error: null, code: 200 };
   }
 }
