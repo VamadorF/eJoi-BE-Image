15673423c8e38a7f03131e4b26d9693c896fb1c8
Jose Pailamilla <jose.pailamilla@mejoradigital.cl>
2026-04-17 17:06:29 -0400

    integracion JWT en llm.image

--- src/llm/llm.controller.ts
+++ src/llm/llm.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { LlmService } from "./llm.service";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+
 
 @Controller("llm")
 export class LlmController {
   constructor(private readonly llm: LlmService) { }
 
   @Post("image")
+  @UseGuards(JwtAuthGuard)
   async imageTest(@Body() body: { prompt: string; companionId: string }) {
     const result = await this.llm.generateAndStoreImage({
       companionId: body?.companionId,
