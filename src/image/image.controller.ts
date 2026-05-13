fb12772ae34127865d1f8a994251e927791be5ea
Javier Fernandez <javier.fernandez@mejoradigital.cl>
2026-05-13 16:25:37 -0400

    fix: deprecated endpoint

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -2,8 +2,8 @@ import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards, UploadedFile, 
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ImageService } from './image.service';
 import { GenerateImageDto } from './dto/generate-image.dto';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'; 
-import { ApiTags} from "@nestjs/swagger";
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiTags } from "@nestjs/swagger";
 import { use } from 'passport';
 import { GenerateImageWithFileDto } from './dto/generate-image-with-file.dto';
 
@@ -12,14 +12,14 @@ import { GenerateImageWithFileDto } from './dto/generate-image-with-file.dto';
 export class ImageController {
     constructor(private readonly imageService: ImageService) { }
 
-    @Post('generate')
+/*     @Post('generate')
     @HttpCode(HttpStatus.OK)
     @UseGuards(JwtAuthGuard)
     async generateImage(@Body() dto: GenerateImageDto) {
         return this.imageService.generateImageDallE(dto);
-    }
+    } */
 
-@Post('generate-with-image')
+    @Post('generate-with-image')
     @HttpCode(HttpStatus.OK)
     @UseGuards(JwtAuthGuard)
     @UseInterceptors(FileInterceptor('image'))
